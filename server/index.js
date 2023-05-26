133948fdbdc21aea49e4e599db2d705a448282b8
Kevin Inzelrac <kevininzelrac@me.com>
2023-05-26 21:04:23 +0200

    update server/index.js

--- server/index.js
+++ server/index.js
@@ -1,50 +1,29 @@
 const express = require("express");
-const awsServerlessExpress = require("aws-serverless-express");
+const serverlessExpress = require("@vendia/serverless-express");
 
 const app = express();
 app.use(express.json());
 
-/* const cors = require("cors");
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-); */
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const cors = require("cors");
+app.use(cors({ origin: "*" }));
 
 app.get("/api/home", (req, res) => {
-  res.send("Hello from Home!");
+  res.send({ data: "Hello from Home!" });
 });
 
 app.get("/api/portfolio", (req, res) => {
-  res.send("Hello from Portfolio!");
+  res.send({ data: "Hello from Portfolio!" });
 });
 
-module.exports = app;
+app.get("/api/contact", (req, res) => {
+  res.send({ data: "Hello from Contact!" });
+});
 
-exports.handler = (event, context) => {
-  // Ajoutez une fonction d'entrée pour votre fonction Lambda
-  const server = awsServerlessExpress.createServer(app); // Créez le serveur Express et passez l'objet event et le contexte
-  awsServerlessExpress.proxy(server, event, context); // Démarrez le serveur Express
-};
-
-// Ajoutez des configurations pour l'environnement de développement ou de production
-if (process.env.NODE_ENV === "dev") {
-  // Exécutez l'application en mode de développement
+if (process.env.NODE_ENV === "development") {
   const port = 3001;
   app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   });
 } else {
-  // Exportez la fonction Lambda
-  module.exports.handler = (event, context) => {
-    // Créez le serveur Express et passez l'objet event et le contexte
-    const server = awsServerlessExpress.createServer(app);
-    // Démarrez le serveur Express
-    awsServerlessExpress.proxy(server, event, context);
-  };
+  exports.handler = serverlessExpress({ app });
 }
